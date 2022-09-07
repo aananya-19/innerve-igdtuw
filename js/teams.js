413f8f09d073d36f7f5aa771cb47f2845d5e66b3
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-09-06 22:29:48 -0700

    images scroll effect

--- js/teams.js
+++ js/teams.js
@@ -1,4 +1,4 @@
-$(window).scroll(function(){
+/*$(window).scroll(function(){
     if(screen.availWidth>=1024)
     {
     for(var i=0;i<3;i++)
@@ -215,4 +215,28 @@ else{for(var i=0;i<3;i++)
             }
 
 }
+});*/
+/* FadeIn Scroll */
+$(document).ready(function() {
+    
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+  
+      /* Check the location of each desired element */
+      $('.container').each( function(i){
+          
+          var bottom_of_object = $(this).position().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},900);
+                  
+          }
+          
+      }); 
+  
+  });
+  
 });
