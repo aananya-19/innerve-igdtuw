e87de01e9617daac75dc1ce7b6513c0cb4a38255
Tanya <tanyadiit7@gmail.com>
2022-09-27 11:53:28 +0530

    pop up done

--- js/landing.js
+++ js/landing.js
@@ -1,35 +1,50 @@
-window.onload = function() {
+const showPopUp = () => {
+  const popup = document.getElementById('PopUp');
+  setTimeout(() => {
+    popup.style.display = "flex"
+  }, 5000)
+}
 
-  window.addEventListener('scroll', function(e) {
-    
+const toggle = () => {
+  const popup = document.getElementById('PopUp');
+  console.log(popup)
+  popup.style.display = 'none';
+}
+
+showPopUp();
+
+window.onload = function () {
+
+  window.addEventListener('scroll', function (e) {
+
     let s = this.scrollY;
     let w = this.outerWidth;
     let h = document.getElementsByClassName('paralax')[0].clientWidth;
     let h_b = document.getElementsByClassName('container')[0].clientWidth;
-    let p = s/h*100;
-    let p_b = s/h_b*100;
-    let opas = 1-1/100*p_b;
+    let p = s / h * 100;
+    let p_b = s / h_b * 100;
+    let opas = 1 - 1 / 100 * p_b;
     let z_1 = 1 + (w / 10000 * p_b);
-    document.getElementsByClassName('p-item4')[0].style= `transform: scale(${z_1});opacity: ${opas}`;
-    let z_2 = 1+(w/5000000*p);
-    document.getElementsByClassName('p-item1')[0].style= `transform: scale(${z_2})`;
-    let hr = w/2000*p_b;
-    let z_3 = 1+(w*0.000005*p_b);
-    document.getElementsByClassName('p-item2')[0].style= `transform: translate3d(${hr}px,0,0) scale(${z_3})`;
-    let hr_2 = w/1500*p_b;
-    let z_4 = 1+(w*0.00001*p_b);
-    document.getElementsByClassName('p-item3')[0].style= `transform: translate3d(${hr_2}px,0,0) scale(${z_4})`;
+    document.getElementsByClassName('p-item4')[0].style = `transform: scale(${z_1});opacity: ${opas}`;
+    let z_2 = 1 + (w / 5000000 * p);
+    document.getElementsByClassName('p-item1')[0].style = `transform: scale(${z_2})`;
+    let hr = w / 2000 * p_b;
+    let z_3 = 1 + (w * 0.000005 * p_b);
+    document.getElementsByClassName('p-item2')[0].style = `transform: translate3d(${hr}px,0,0) scale(${z_3})`;
+    let hr_2 = w / 1500 * p_b;
+    let z_4 = 1 + (w * 0.00001 * p_b);
+    document.getElementsByClassName('p-item3')[0].style = `transform: translate3d(${hr_2}px,0,0) scale(${z_4})`;
   })
-  
+
 }
 
 var words = [
   "INNERVE'22"
 ];
 var letters = "abcdefghijklmnopqrstuvwxyz#%&^+=-",
-    speed = 250,
-    steps = 4,
-    loader = document.querySelector('#loader');
+  speed = 250,
+  steps = 4,
+  loader = document.querySelector('#loader');
 
 function getRandomWord() {
   var randomWord = words[Math.floor(Math.random() * words.length)];
@@ -43,16 +58,16 @@ function getRandomLetter() {
 function randomWordLoop() {
   var word = getRandomWord();
   var textLength = word.length;
-  for(var i = 0; i < textLength; i++) {    
-    (function(i,word){
+  for (var i = 0; i < textLength; i++) {
+    (function (i, word) {
       letterAppear(i, word);
-    })(i,word)
+    })(i, word)
   }
-  
+
   function letterAppear(i, word) {
-    setTimeout(function() {
+    setTimeout(function () {
       randomLetters(i, word);
-    }, speed*i);  
+    }, speed * i);
   }
 
   function randomLetters(i, word) {
@@ -62,46 +77,46 @@ function randomWordLoop() {
   }
 
   function charsAnim(i, word, j) {
-    setTimeout(function() {
-      var count = j; 
-      if (j < steps) {           
+    setTimeout(function () {
+      var count = j;
+      if (j < steps) {
         randomChar(i, word, count, j);
       } else {
         goodChar(i, word, count, j);
       }
       /* seems it fails less if I divide j, don't know why */
       /*}, (speed/steps)*(j / 1.8));*/
-    }, ((speed/steps)*j) - (speed/steps));
+    }, ((speed / steps) * j) - (speed / steps));
   }
 
   function randomChar(i, word, count, j) {
-    var letter = getRandomLetter();    
+    var letter = getRandomLetter();
     if (j > 0) {
       var oldText = loader.textContent.slice(0, -1);
     } else {
       var oldText = loader.textContent;
     }
-    loader.textContent = oldText + letter;    
+    loader.textContent = oldText + letter;
   }
   function goodChar(i, word, count, j) {
-    var oldText = loader.textContent.slice(0, -1);  
+    var oldText = loader.textContent.slice(0, -1);
     loader.textContent = oldText + word[i];
-    if (i == textLength - 1 ) {
+    if (i == textLength - 1) {
       removeWord();
     }
   }
-  
+
   function removeWord() {
-    setTimeout(function() {
+    setTimeout(function () {
       for (var k = 0; k < textLength; k++) {
-         removeLetters(k);
+        removeLetters(k);
       }
-    }, speed*2);
+    }, speed * 2);
   }
   function removeLetters(k) {
-    setTimeout(function() {
+    setTimeout(function () {
       removeLetter(k);
-    }, 75*k);
+    }, 75 * k);
   }
   function removeLetter(k) {
     var actualText = loader.textContent.slice(0, -1);
